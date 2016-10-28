ad9ccb5c2bd7ae7db6e4e989b3fe70c133453075
DannyBoyNYC <daniel.deverell@gmail.com>
2016-10-28 17:05:47 -0400

    Module finished

--- 1-angular-animation/3-angular-anim/manage-list/manage-list.component.js
+++ 1-angular-animation/3-angular-anim/manage-list/manage-list.component.js
@@ -1,6 +1,18 @@
 angular.module('myApp').component('manageList', {
 
-	templateUrl: 'manage-list/manage-list.template.html',
+	template:
+	`
+		<h1>Pirate Day</h1>
+		<ul>
+			<li ng-repeat="item in $ctrl.items" class="fade">
+				{{ item.name }}
+				<span ng-click="$ctrl.removeItem($index)">X</span>
+			</li>
+		</ul>
+
+		<input type="text" ng-model="$ctrl.item.name" />
+		<button ng-click="$ctrl.addItem()">Add Item</button>
+	`,
 
 	controller: function ItemCtrl() {
 
